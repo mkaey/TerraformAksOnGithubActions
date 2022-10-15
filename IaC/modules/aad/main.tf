815b7d8250e61c0ab7d28f60f8af11f34b6c30f0
mkaey <mkaettner@gmail.com>
2022-10-15 19:15:13 +0200

    Added API Permissions

--- IaC/modules/aad/main.tf
+++ IaC/modules/aad/main.tf
@@ -12,6 +12,23 @@ resource "azuread_application" "githubApp" {
   identifier_uris  = ["api://githubactions"]
   owners           = [data.azuread_client_config.current.object_id]
   sign_in_audience = "AzureADMyOrg"
+
+  required_resource_access {
+    resource_app_id = "00000003-0000-0000-c000-000000000000"
+
+    resource_access {
+      id   = "62a82d76-70ea-41e2-9197-370581804d09" # Group.Write.All
+      type = "Role"
+    }
+    resource_access {
+
+      id   = "df021288-bdef-4463-88db-98f22de89214" # User.Read.All
+      type = "Role"
+
+    }
+  }
+
+
 }
 
 resource "azuread_service_principal" "gitHubSp" {
